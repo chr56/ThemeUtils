534ba30c832bca1c7d9df23da0410920a5e7c119
chr56 <chr0056@gmail.com>
2024-03-16 13:53:41 +0800

    [libMaterials] import from
    MaterialTools/libColorRes

---
+++ libMaterials/src/main/java/util/theme/materials/IColor.java
@@ -0,0 +1,15 @@
+package util.theme.materials;
+
+/**
+ * IColor
+ * <p>
+ * Created by mikepenz on 07.07.15.
+ * @noinspection unused
+ */
+interface IColor {
+    String getAsString();
+
+    int getAsColor();
+
+    int getAsResource();
+}

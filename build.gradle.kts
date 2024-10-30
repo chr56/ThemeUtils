890fd59b344244930665f3651103de5f7d82c6d3
chr56 <chr0056@gmail.com>
2024-10-30 10:28:44 +0800

    [lib] release 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ tasks.create(name = "clean", type = Delete::class) {
     doLast { delete(rootProject.layout.buildDirectory) }
 }
 
-val libVersion: String by extra("0.2.0")
+val libVersion: String by extra("0.3.0")
 val libGroup: String by extra("com.github.chr56")
 val libNamespacePrefix: String by extra("util.theme")

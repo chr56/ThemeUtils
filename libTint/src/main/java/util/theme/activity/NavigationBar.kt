3d077ec597d080e29899fe60ac0beaf9e18c4b4b
chr56 <chr0056@gmail.com>
2024-11-21 16:10:02 +0800

    [NavigationBarTint] use `WindowInsetsControllerCompat` & support stub

--- libTint/src/main/java/util/theme/activity/NavigationBar.kt
+++ libTint/src/main/java/util/theme/activity/NavigationBar.kt
@@ -1,49 +1,29 @@
 @file:Suppress("unused")
+
 package util.theme.activity
 
 import android.app.Activity
-import android.os.Build
 import android.view.View
-import android.view.WindowInsetsController
 import androidx.annotation.ColorInt
+import androidx.core.view.WindowInsetsControllerCompat
 import util.theme.color.isColorLight
 
 /**
- * @param color NavigationBar background color
+ * @param color navigation bar background color
+ * @param stubViewId stub view of statusbar if you have any
  */
-fun Activity.setNavigationBarColor(@ColorInt color: Int) {
-    this.window.navigationBarColor = color
+fun Activity.setNavigationBarColor(@ColorInt color: Int, stubViewId: Int = 0) {
+    with(window) {
+        window.navigationBarColor = color
+        if (stubViewId != 0) {
+            decorView.rootView.findViewById<View>(stubViewId)?.setBackgroundColor(color)
+        }
+    }
     adjustNavigationBarButtons(color)
 }
 
-fun Activity.adjustNavigationBarButtons(bgColor: Int) {
-    adjustNavigationBarButtons(isColorLight(bgColor))
-}
+fun Activity.adjustNavigationBarButtons(bgColor: Int) = adjustNavigationBarButtons(isColorLight(bgColor))
 
 fun Activity.adjustNavigationBarButtons(enabled: Boolean) {
-    @Suppress("DEPRECATION")
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        this.window.insetsController?.let { insetsController ->
-            if (enabled) {
-                insetsController.setSystemBarsAppearance(
-                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
-                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                )
-            } else {
-                insetsController.setSystemBarsAppearance(
-                    0,
-                    WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
-                )
-            }
-        }
-    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        this.window.decorView.run {
-            val visibility = if (enabled) {
-                systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            } else {
-                systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
-            }
-            systemUiVisibility = visibility
-        }
-    }
+    WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightNavigationBars = enabled
 }
